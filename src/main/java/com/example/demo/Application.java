1e6e2777f7e3fa87a6b6c3185e10a8f7e793bec3
Dillip kumar sahoo <dillipgit@gmail.com>
2020-05-28 21:43:41 +0530

    pull test 2

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Application.main()");
+               int x = 10;
 	}
 
 }
